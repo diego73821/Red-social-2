4eb299d76c08e3078fb24aa6b77c06f95cf06885
diego73821 <diego73821@gmail.com>
2022-10-06 12:46:27 -0500

    cambios

--- angular/src/app/components/login/login.component.ts
+++ angular/src/app/components/login/login.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router'
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
   public title: string;
+  public user: User
 
-  constructor() {
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService
+  ) {
     this.title = 'Identificate'
+    this.user = new User('','','','','','','ROLE_USER','')
    }
 
   ngOnInit(): void {
     console.log('Componente de login cargado')
+  }
+
+  onSubmit(){
+    alert(this.user.email)
+    alert(this.user.password)
+    console.log(this.user)
   }
 
 }
--- angular/src/app/services/user.service.ts
+++ angular/src/app/services/user.service.ts
@@ -11,9 +11,11 @@ export class UserService{
     constructor(public _http: HttpClient){
         this.url= GLOBAL.url
     }
-    register(user_to_register: any){
-        console.log(user_to_register)
-        console.log(this.url)
+    register(user: User): Observable<any>{
+        let params = JSON.stringify(user)
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+
+        return this._http.post(this.url+'register', params, {headers: headers})
     }
 }
 
